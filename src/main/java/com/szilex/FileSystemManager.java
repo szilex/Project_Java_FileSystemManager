6dfa2e1c7e390e3707499e9d2e7bba8099d28f54
szilex <32484638+szilex@users.noreply.github.com>
2021-10-30 17:17:52 +0200

    Create basic project structure

---
+++ src/main/java/com/szilex/FileSystemManager.java
@@ -0,0 +1,7 @@
+package com.szilex;
+
+public class FileSystemManager {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/szilex/FileSystemManagerTests.java
@@ -0,0 +1,4 @@
+package com.szilex;
+
+public class FileSystemManagerTests {
+}
